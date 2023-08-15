059e6a74db17a1b20660f99cf8c020e68c68cf78
Ahmed Matter <amatter705@gmail.com>
2023-08-15 16:15:48 -0700

    1-alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+
+/**
+ * print_alphabet - make the alphabet
+ * Return: void
+ */
+
+void print_alphabet(void)
+{
+
+	char c;
+	for (c = 'a'; c<= 'z'; c++)
+	{
+		putchar(c);
+	}
+	_putchar('\n');
+}
